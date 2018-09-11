858ddadac74ff1e2d130a70abf98a21fd902b0c5
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2018-09-11 09:30:33 +0200

    redirect in 404 page and change the code for array

--- product.php
+++ product.php
@@ -1,47 +1,55 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="fr">
 
 <head>
   <?php
   $productIndex = $_GET['index'];
 
-    $product = [
-      ['name' => 'Nike Air Span II SE Mini Swoosh - Black/Red',
-      'price' => '109,00 €',
-      'available' => 'Disponible',
-      'descript' => "Une fois de plus, la marque américaine présente une nouvelle version d’une silhouette lancée à la fin des années 80, la Nike Air Span 2. Ce modèle aux lignes rétro arbore une construction associant différents matériaux comme du mesh, de la feutrine et du cuir. Côté coloris, cette édition présente une association de noir et de détails rouges apportant du contraste à l’ensemble à l’image du mini-Swoosh. Une semelle blanc cassé finalise son design. Cette Nike Air Span II SE Mini Swoosh Black/Red fait partie de la collection automne/hiver 2018.",
-      'src' => "img/air-span.jpg",
-      'size' => [40, 42, 42.5, 43, 44, 44.5],
-      'color' => ['black', 'red']
-      ],
-        [
-        'name' => 'New Balance M1500.9 FT - Made in UK',
-        'price' => '179,00 €',
-        'available' => 'Disponible',
-        'descript' => "Les équipes de la marque américaine ont revu le design de l’emblématique New Balance 1500 en lui apportant un côté plus technique avec cette semelle empruntée à la NB990v3. Ce changement confère un look tout de suite plus moderne à la silhouette. Son assemblage a été réalisé dans l’usine anglaise de la marque à Flimby avec ce mélange de cuir suédé et de mesh. Aux niveaux des couleurs, la paire affiche un mix de différents bleus, de rose, de blanc et de gris. Cette New Balance M1500.9 FT, Made in UK, appartient à la collection Automne/Hiver 2018.",
-        'src' => "img/new-balance-made-in-uk.jpg",
-        'size' => [40, 41.5, 42.5, 43, 44, 44.5, 45],
-        'color' => ['white', 'lightblue', 'lightpink']
-        ],
-        [
-        'name' => 'Asics Gel Saga - White/Asics Blue',
-        'price' => '109,00 €',
-        'available' => 'Disponible',
-        'descript' => "Pour sa nouvelle collection, la firme japonaise relance l'Asics Gel Saga dans son coloris d'origine. Initialement lancé en 1991, le modèle arbore une empeigne mêlant du mesh à des empiècements en feutrine comme à son origine. Côté coloris, cette édition se pare d’une teinte blanche accompagnée par des touches de bleu et d’orange. Le tout est posé sur une semelle blanche couplée à une semelle d’usure reprenant les teintes de l’empeigne. Cette Asics Gel Saga White/Asics Blue est issue de la collection Automne/Hiver 2018.",
-        'src' => "img/gel-saga-whiteasics-blue.jpg",
-        'size' => [40, 41.5, 42.5, 43.5, 44, 44.5, 45, 46],
-        'color' => ['white', 'blue', 'orange']
-      ],
-        [
-        'name' => 'Reebok ACT 300 MU - Chalk/Teal Energy',
-        'price' => '99,00 €',
-        'available' => 'Disponible',
-        'descript' => "Reebok propose une version très sobre de la Reebok ACT 300. L’édition est parée d’un cuir entièrement beige. On remarque que l’ensemble des logos sont dans une teinte verte. Ces détails confèrent à cette édition un look vintage. Une bande verte, sur l’arrière du modèle, vient apporter du contraste à l’ensemble. Enfin une semelle monochrome beige parachève le design de cette itération. Cette Reebok ACT 300 MU Chalk/Teal Energy appartient à la collection Automne/Hiver 2018.",
-        'src' => "img/act-300-mu-chalkteal-energy.jpg",
-        'size' => [45],
-        'color' => ['white', 'lightgreen']
-      ]
-    ];
+
+
+        $product = [
+          ['name' => 'Nike Air Span II SE Mini Swoosh - Black/Red',
+          'price' => '109,00 €',
+          'available' => 'Disponible',
+          'descript' => "Une fois de plus, la marque américaine présente une nouvelle version d’une silhouette lancée à la fin des années 80, la Nike Air Span 2. Ce modèle aux lignes rétro arbore une construction associant différents matériaux comme du mesh, de la feutrine et du cuir. Côté coloris, cette édition présente une association de noir et de détails rouges apportant du contraste à l’ensemble à l’image du mini-Swoosh. Une semelle blanc cassé finalise son design. Cette Nike Air Span II SE Mini Swoosh Black/Red fait partie de la collection automne/hiver 2018.",
+          'src' => "img/air-span.jpg",
+          'size' => [40, 42, 42.5, 43, 44, 44.5],
+          'color' => ['black', 'red']
+          ],
+            [
+            'name' => 'New Balance M1500.9 FT - Made in UK',
+            'price' => '179,00 €',
+            'available' => 'Disponible',
+            'descript' => "Les équipes de la marque américaine ont revu le design de l’emblématique New Balance 1500 en lui apportant un côté plus technique avec cette semelle empruntée à la NB990v3. Ce changement confère un look tout de suite plus moderne à la silhouette. Son assemblage a été réalisé dans l’usine anglaise de la marque à Flimby avec ce mélange de cuir suédé et de mesh. Aux niveaux des couleurs, la paire affiche un mix de différents bleus, de rose, de blanc et de gris. Cette New Balance M1500.9 FT, Made in UK, appartient à la collection Automne/Hiver 2018.",
+            'src' => "img/new-balance-made-in-uk.jpg",
+            'size' => [40, 41.5, 42.5, 43, 44, 44.5, 45],
+            'color' => ['white', 'lightblue', 'lightpink']
+            ],
+            [
+            'name' => 'Asics Gel Saga - White/Asics Blue',
+            'price' => '109,00 €',
+            'available' => 'Disponible',
+            'descript' => "Pour sa nouvelle collection, la firme japonaise relance l'Asics Gel Saga dans son coloris d'origine. Initialement lancé en 1991, le modèle arbore une empeigne mêlant du mesh à des empiècements en feutrine comme à son origine. Côté coloris, cette édition se pare d’une teinte blanche accompagnée par des touches de bleu et d’orange. Le tout est posé sur une semelle blanche couplée à une semelle d’usure reprenant les teintes de l’empeigne. Cette Asics Gel Saga White/Asics Blue est issue de la collection Automne/Hiver 2018.",
+            'src' => "img/gel-saga-whiteasics-blue.jpg",
+            'size' => [40, 41.5, 42.5, 43.5, 44, 44.5, 45, 46],
+            'color' => ['white', 'blue', 'orange']
+          ],
+            [
+            'name' => 'Reebok ACT 300 MU - Chalk/Teal Energy',
+            'price' => '99,00 €',
+            'available' => 'Disponible',
+            'descript' => "Reebok propose une version très sobre de la Reebok ACT 300. L’édition est parée d’un cuir entièrement beige. On remarque que l’ensemble des logos sont dans une teinte verte. Ces détails confèrent à cette édition un look vintage. Une bande verte, sur l’arrière du modèle, vient apporter du contraste à l’ensemble. Enfin une semelle monochrome beige parachève le design de cette itération. Cette Reebok ACT 300 MU Chalk/Teal Energy appartient à la collection Automne/Hiver 2018.",
+            'src' => "img/act-300-mu-chalkteal-energy.jpg",
+            'size' => [45],
+            'color' => ['white', 'lightgreen']
+          ]
+        ];
+        if (! isset($productIndex) OR $productIndex >= count($product)) {
+          header('Location: 404.html');
+          exit;
+        }
+
+
 
 
 
@@ -49,7 +57,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title><?php echo $product[$productIndex]["name"]; ?> | Basket Addict</title>
-  <meta name="description" content="">
+  <meta name="description" content="<?php echo $product[$productIndex]['descript']; ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <link rel="manifest" href="site.webmanifest">
@@ -94,18 +102,18 @@
 
       <div class="row">
         <p class="col-12 mt-2 mb-1 text-secondary font-weight-bold">Couleur</p>
-        <?php  for ($number = 0; $number <  count($color); $number++) {
-
-            echo '<div class="color col-1 ' . $color[$number] . '"></div>';
+        <?php  foreach($product[$productIndex]['color'] as $element)
+{
+            echo '<div class="color col-1 ' . $element . '"></div>';
 
           }
         ?>
 
       </div>
 
-      <p class="font-weight-bold available"><?php echo $product[0]['available']; ?></p>
+      <p class="font-weight-bold available"><?php echo $product[$productIndex]['available']; ?></p>
       <p class="text-secondary font-weight-bold">Description</p>
-      <p class="text-secondary"><?php echo $product[0]['descript']; ?></p>
+      <p class="text-secondary"><?php echo $product[$productIndex]['descript']; ?></p>
     </section>
 
   </main>
